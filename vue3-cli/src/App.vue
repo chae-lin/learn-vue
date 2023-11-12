efddf6043a66516d7057d19a5a85fb0aecbada60
chaelin kim <gjf0055@gmail.com>
2023-11-12 23:47:35 +0900

    se5. Vue CLI: 뷰 프로젝트 생성 도구 se4. Vue 싱글 파일 컴포넌트

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <AppHeader v-bind:appTitle="massage" v-on:change="changeMassage"></AppHeader>
+  <!-- v-on: 은 @ 로 축약가능 -->
+  <button type="button" @click="showAlert">경고</button>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: {
+    // '컴포넌트 이름': 컴포넌트 내용
+    // AppHeader: AppHeader, (키와 벨류가 똑같을 때 하나의 단어로 축약 가능)
+    AppHeader,
+  },
+  data() {
+    return {
+      massage: "앱해더 프롭스 초기 값",
+    };
+  },
+  methods: {
+    showAlert() {
+      alert("hello");
+    },
+    changeMassage() {
+      this.massage = "앱해더 프롭스를 변경됨";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ vue3-cli/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ appTitle }}</h1>
+  <button @click="changeTitle">데이터 변경</button>
+</template>
+
+<script>
+export default {
+  props: ["appTitle"],
+  methods: {
+    changeTitle() {
+      this.$emit("change");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
