22ac12ad39c2c9299e943c400f4c8d0847326082
chaelin kim <gjf0055@gmail.com>
2024-01-24 12:36:40 +0900

    Vue Composition API 기초

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <AppHeader v-bind:appTitle="massage" v-on:change="changeMassage"></AppHeader>
   <!-- v-on: 은 @ 로 축약가능 -->
+  <AppHeader v-bind:appTitle="massage" @change="changeMassage"></AppHeader>
   <button type="button" @click="showAlert">경고</button>
 </template>
 
-<script>
+<script setup>
+// ## composition 으로 변경하기
+
+// 방법1.
+// setup() {
+//   const massage = ref("앱해더 프롭스 초기 값");
+//   return( massage )
+// }
+
+import { ref } from "vue";
+import AppHeader from "./components/AppHeader.vue";
+
+const massage = ref("앱해더 프롭스 초기 값");
+
+const changeMassage = () => {
+  massage.value = "앱해더 프롭스를 변경됨";
+};
+
+const showAlert = () => {
+  alert("hello");
+};
+</script>
+
+<!-- options <script>
 import AppHeader from "./components/AppHeader.vue";
 
 export default {
@@ -27,6 +50,4 @@ export default {
     },
   },
 };
-</script>
-
-<style scoped></style>
+</script> -->
